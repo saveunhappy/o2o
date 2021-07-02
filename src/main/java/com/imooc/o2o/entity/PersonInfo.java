98a5e1337762205fd6aa4282b62d99651e6d2e57
JiangTaoHou <925147954@qq.com>
2021-07-03 00:59:15 +0800

    Add system version 1.0 entity and maven dependencies

---
+++ src/main/java/com/imooc/o2o/entity/PersonInfo.java
@@ -0,0 +1,27 @@
+package com.imooc.o2o.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class PersonInfo {
+    //用户id
+    private Long userId;
+    //用户名称
+    private String name;
+    //用户头像，是一个url
+    private String profileImage;
+    //用户邮箱
+    private String email;
+    //用户性别
+    private String gender;
+    //是否被禁用，0是被禁用，1是可使用，默认是0
+    private Integer enableStatus;
+    //1.顾客 2.店家 3.超级管理员
+    private Integer userType;
+    //创建时间
+    private Date createTime;
+    //修改时间
+    private Date lastEditTime;
+}
