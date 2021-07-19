0e6597cdfa2d2435f8449d7eba10667bce6980c8
JiangTaoHou <925147954@qq.com>
2021-07-19 14:47:16 +0800

    rebase

--- src/main/java/com/imooc/o2o/dao/ProductCategoryDao.java
+++ src/main/java/com/imooc/o2o/dao/ProductCategoryDao.java
@@ -2,7 +2,6 @@ package com.imooc.o2o.dao;
 
 import com.imooc.o2o.entity.ProductCategory;
 import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
 
 public interface ProductCategoryDao {
