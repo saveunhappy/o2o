c9e81b407d1b99be6897b752258ad23404a7042b
JiangTaoHou <925147954@qq.com>
2021-07-23 23:34:28 +0800

    fix get currentUser hard Code

--- src/main/java/com/imooc/o2o/controller/shopadmin/ShopManagementController.java
+++ src/main/java/com/imooc/o2o/controller/shopadmin/ShopManagementController.java
@@ -76,21 +76,19 @@ public class ShopManagementController {
     @ResponseBody
     private Map<String, Object> getShopList(HttpServletRequest request) {
         Map<String, Object> modelMap = new HashMap<>();
-        PersonInfo user = new PersonInfo();
-        user.setUserId(1L);
-        user.setName("test");
-        request.getSession().setAttribute("user", user);
-        user = (PersonInfo) request.getSession().getAttribute("user");
+        PersonInfo user = (PersonInfo) request.getSession().getAttribute("user");
         try {
             Shop shopCondition = new Shop();
             shopCondition.setOwner(user);
             ShopExecution se = shopService.getShopList(shopCondition, 0, 100);
             modelMap.put("shopList", se.getShopList());
+            // 列出店铺成功之后，将店铺放入session中作为权限验证依据，即该帐号只能操作它自己的店铺
+            request.getSession().setAttribute("shopList", se.getShopList());
             modelMap.put("user", user);
             modelMap.put("success", true);
         } catch (Exception e) {
             modelMap.put("success", false);
-            modelMap.put("errorMsg", e.getMessage());
+            modelMap.put("errMsg", e.getMessage());
         }
         return modelMap;
     }
